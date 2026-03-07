f2cbc03f584e29fe1699828a01ae520d871c3f64
Vasco Nunes <vasco.m.nunes@me.com>
2026-03-07 09:21:23 +0000

    Refactor: Improve LookAndFeel and Knob implementation
    - Move some LookAndFeel methods to the header for cleaner organization.
    - Adjust knob bounds calculation to ensure tighter integration with the
      UI.
    - Introduce `getSliderPopupPlacement` and `drawBubble` for improved
      tooltip appearance.

--- include/PlugUI/PlugLookAndFeel.h
+++ include/PlugUI/PlugLookAndFeel.h
@@ -2,42 +2,40 @@
 
 #include <juce_gui_basics/juce_gui_basics.h>
 
-namespace PlugUI {
+namespace PlugUI
+{
 
-class PlugLookAndFeel : public juce::LookAndFeel_V4 {
+class PlugLookAndFeel : public juce::LookAndFeel_V4
+{
 public:
-  PlugLookAndFeel();
+    PlugLookAndFeel();
 
-  void drawRotarySlider(juce::Graphics &, int x, int y, int width, int height,
-                        float sliderPos, float rotaryStartAngle,
-                        float rotaryEndAngle, juce::Slider &) override;
-  void drawLinearSlider(juce::Graphics &, int x, int y, int width, int height,
-                        float sliderPos, float minSliderPos, float maxSliderPos,
-                        juce::Slider::SliderStyle, juce::Slider &) override;
+    void drawRotarySlider (juce::Graphics&, int x, int y, int width, int height, float sliderPos,
+                           float rotaryStartAngle, float rotaryEndAngle, juce::Slider&) override;
+    void drawLinearSlider (juce::Graphics&, int x, int y, int width, int height, float sliderPos, float minSliderPos,
+                           float maxSliderPos, juce::Slider::SliderStyle, juce::Slider&) override;
 
-  void drawToggleButton(juce::Graphics &, juce::ToggleButton &,
-                        bool shouldDrawButtonAsHighlighted,
-                        bool shouldDrawButtonAsDown) override;
-  void drawButtonBackground(juce::Graphics &, juce::Button &,
-                            const juce::Colour &backgroundColour,
-                            bool shouldDrawButtonAsHighlighted,
-                            bool shouldDrawButtonAsDown) override;
-  void drawButtonText(juce::Graphics &, juce::TextButton &,
-                      bool shouldDrawButtonAsHighlighted,
-                      bool shouldDrawButtonAsDown) override;
+    void drawToggleButton (juce::Graphics&, juce::ToggleButton&, bool shouldDrawButtonAsHighlighted,
+                           bool shouldDrawButtonAsDown) override;
+    void drawButtonBackground (juce::Graphics&, juce::Button&, const juce::Colour& backgroundColour,
+                               bool shouldDrawButtonAsHighlighted, bool shouldDrawButtonAsDown) override;
+    void drawButtonText (juce::Graphics&, juce::TextButton&, bool shouldDrawButtonAsHighlighted,
+                         bool shouldDrawButtonAsDown) override;
 
-  void drawLabel(juce::Graphics &, juce::Label &) override;
-  void drawComboBox(juce::Graphics &, int width, int height, bool, int buttonX,
-                    int buttonY, int buttonW, int buttonH,
-                    juce::ComboBox &) override;
-  juce::Font getComboBoxFont(juce::ComboBox &) override;
-  void positionComboBoxText(juce::ComboBox &, juce::Label &) override;
-  juce::Font getSliderPopupFont(juce::Slider &) override;
-  juce::Font getLabelFont(juce::Label &) override;
-  juce::Label *createSliderTextBox(juce::Slider &) override;
+    void drawLabel (juce::Graphics&, juce::Label&) override;
+    void drawComboBox (juce::Graphics&, int width, int height, bool, int buttonX, int buttonY, int buttonW, int buttonH,
+                       juce::ComboBox&) override;
+    juce::Font getComboBoxFont (juce::ComboBox&) override;
+    void positionComboBoxText (juce::ComboBox&, juce::Label&) override;
+    juce::Font getSliderPopupFont (juce::Slider&) override;
+    int getSliderPopupPlacement (juce::Slider&) override;
+    void drawBubble (juce::Graphics&, juce::BubbleComponent&, const juce::Point<float>& tip,
+                     const juce::Rectangle<float>& body) override;
+    juce::Font getLabelFont (juce::Label&) override;
+    juce::Label* createSliderTextBox (juce::Slider&) override;
 
 private:
-  JUCE_DECLARE_NON_COPYABLE_WITH_LEAK_DETECTOR(PlugLookAndFeel)
+    JUCE_DECLARE_NON_COPYABLE_WITH_LEAK_DETECTOR (PlugLookAndFeel)
 };
 
 } // namespace PlugUI
